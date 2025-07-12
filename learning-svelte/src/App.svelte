a93feee8f600f8e706b7c89269b398abe93d3cae
Brittani Lovelace <blovelace84@gmail.com>
2025-07-12 19:37:25 -0400

    working with strings

--- learning-svelte/src/App.svelte
+++ learning-svelte/src/App.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
   let name = 'Brittani'
   let src = '/sonic.jpg';
+  let string = `this string contains some <strong>HTML!!!</strong>`;
   import Nested from "./Nested.svelte";
 </script>
 
 <p>This is a paragraph</p>
+<p>{@html string}</p>
 <Nested />
 <h1>Hello, {name.toUpperCase()}</h1>
 <img src={src} alt="{name}" />
