999d3a630c8a2622d10d6fac006e239e267c7fdc
Brittani Lovelace <blovelace84@gmail.com>
2025-07-12 19:34:40 -0400

    Initial commit

---
+++ learning-svelte/src/App.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  let name = 'Brittani'
+  let src = '/sonic.jpg';
+  import Nested from "./Nested.svelte";
+</script>
+
+<p>This is a paragraph</p>
+<Nested />
+<h1>Hello, {name.toUpperCase()}</h1>
+<img src={src} alt="{name}" />
+
+<style>
+  p{
+    color: blue;
+    font-family: 'Comic Sans MS', cursive;
+    font-size: 2em;
+  }
+</style>
