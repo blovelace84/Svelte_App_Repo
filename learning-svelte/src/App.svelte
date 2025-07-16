f1ffe88794c9c07179691b739ed6f66ce640df66
Brittani Lovelace <blovelace84@gmail.com>
2025-07-16 19:29:05 -0400

    else if statements

--- learning-svelte/src/App.svelte
+++ learning-svelte/src/App.svelte
@@ -51,8 +51,10 @@
 </button>
 {#if count > 10}
 <p>{count} is greater than 10</p>
+{:else if count < 5}
+<p>{count} is less than 5</p>
 {:else}
-<p>{count} is between 0 10</p>
+  <p>{count} is between 5 and 10</p>
 {/if}
 <p>{numbers.join(' + ')} = {total}</p>
 <button onclick={addNumber}>
